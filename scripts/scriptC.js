441aa093bf8024a26dfa04e91e765ce344e01bba
francl lorgere <florgere@aol.com>
2022-02-27 16:36:30 +0100

    change carre cercle script

--- scripts/scriptC.js
+++ scripts/scriptC.js
@@ -1,7 +1,28 @@
 //build square calculation
+let calculer = document.getElementById("calculer");
 
-const result = document.querySelector('mousEvent');
+function event() {
+    alert("ca marche")
+}
+calculer.addEventListener("click", event)
+/*
+const result = document.querySelector('keydown');
 
 document.getElementById('input').addEventListener("input", (e) => {
     console.log(e.target.value);
-})
+})*/
+
+/*dans la fonction  recuperer le chiffre 
+calcule
+affichage dans result*/
+/*
+let figures = "";
+
+
+function calcul("input", (e) => {
+    figures = keyNoteenter
+}
+
+
+result.document.getElemntById("result.target.value")
+*/

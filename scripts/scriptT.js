487f8a1adbf64e504fee808eaf2dd3925e6ceb91
francl lorgere <florgere@aol.com>
2022-02-27 15:51:18 +0100

    change index scriptT trangle

--- scripts/scriptT.js
+++ scripts/scriptT.js
@@ -1,3 +1,15 @@
-let L = prompt("rentrer la base du triangle rectangle");
-let l = prompt("rentrer la hauteur du triangle rectangle");
-alert("resultat: " + L * l / 2);
+//calcule for triangle rectangle
+triangleRectangle.onclick = function triangleRectangle() {
+    let B = prompt("rentrer la base du triangle rectangle");
+    let H = prompt("rentrer la hauteur du triangle rectangle");
+    let calcu = (B * H / 2);
+    console.log(calcu);
+}
+
+triangleIsocele.onclick = function triangleIsocele() {
+    let a = prompt("rentrer le coté gauche ou droite");
+    let b = prompt("rentrer la base du triangle");
+    let calcu = ((a + a) + b);
+    console.log(calcu);
+}
+

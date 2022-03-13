1ad9409be746fd0875fde4fa9f7f51ce9f200932
francl lorgere <florgere@aol.com>
2022-03-13 18:18:40 +0100

    change jeux html script and style

---
+++ scripts/scriptjeuxdevoir.js
@@ -0,0 +1,8 @@
+const score = document.querySelector("score");
+const btn = document.querySelector("button");
+
+
+btn.addEventListener("click", (e) => {
+    console.log(e.target);
+    //score.innerHTML = Math.floor(Math.random() * 6);
+})

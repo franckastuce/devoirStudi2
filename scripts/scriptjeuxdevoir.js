a26cb19a041df245ed7c4d9c25045c4c01dd8d44
francl lorgere <florgere@aol.com>
2022-03-15 14:02:34 +0100

    change htm script style devoir and insert doc dés in images

--- scripts/scriptjeuxdevoir.js
+++ scripts/scriptjeuxdevoir.js
@@ -1,8 +1,21 @@
-const score = document.querySelector("score");
-const btn = document.querySelector("button");
+//random number
+let round1 = document.getElementById("round1");
+let de = document.getElementById("de");
+const boutton = document.getElementById("roll");
+const reset = document.getElementById("reset");
 
+boutton.addEventListener("click", () => {
+    console.log("yes");
+    de.innerHTML = Math.floor(Math.random() * 10);
+    round1.innerHTML = de.innerHTML;
 
-btn.addEventListener("click", (e) => {
-    console.log(e.target);
-    //score.innerHTML = Math.floor(Math.random() * 6);
+    if (de.innerHTML === "1") {
+        alert("AH AH, tu as fait '1', ton compteur reviens a ZERO, passe ton tour")
+        return round1.innerHTML = "0";
+    }
+});
+// reset button
+reset.addEventListener("click", () => {
+    return round1.innerHTML = "0";
 })
+
